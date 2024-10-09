aa973ebcb618c563ac523c42f088e70ff47aed26
4kpros <prosper.abouar@gmail.com>
2024-10-09 13:07:54 +0100

    fix: update openapi header

--- common/constants/openapi.go
+++ common/constants/openapi.go
@@ -2,4 +2,4 @@ package constants
 
 const OPEN_API_VERSION = "1.0.0"
 const OPEN_API_TITLE = "API reference"
-const OPEN_API_DESCRIPTION = "Welcome to the API reference \nThis wil help you"
+const OPEN_API_DESCRIPTION = "Welcome to the API reference. Includes detailed documentation, error handling, and examples."
