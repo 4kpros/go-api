d5a7fb6fa49aa0d2776219b8d77bd900d73d10e7
4kpros <prosper.abouar@gmail.com>
2024-10-11 14:06:06 +0100

    fix: fix password checker

--- common/utils/pagination.go
+++ common/utils/pagination.go
@@ -1,28 +1,38 @@
 package utils
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/4kpros/go-api/common/types"
 	"gorm.io/gorm"
 )
 
-func PaginationScope(model interface{}, pagination *types.Pagination, filters *types.Filter, db *gorm.DB) func(db *gorm.DB) *gorm.DB {
+// Return *gorm.DB pointer with applied search, filter and pagination
+//
+// - Search performs a full-text search on every row of the table. It searches all fields.
+// The official documentation for full text search in PostgreSQL can be found here
+// https://www.postgresql.org/docs/17/textsearch-tables.html#TEXTSEARCH-TABLES-SEARCH
+//
+// - Filter applies an ORDER BY clause to the specified field name,
+// sorting the results in ascending or descending based on the Sort parameter.
+//
+// - Pagination applies an offset and limit to the results, determining which subset of data to display.
+func PaginationScope(model any, pagination *types.Pagination, filter *types.Filter, db *gorm.DB) func(*gorm.DB) *gorm.DB {
 	var count int64
 	db.Model(model).Count(&count)
 	pagination.UpdateFields(&count)
 	return func(db *gorm.DB) *gorm.DB {
-		return db.Offset(pagination.Offset).Limit(pagination.Limit).Order(filters.OrderBy + " " + filters.Sort)
+		return db.Offset(pagination.Offset).Limit(pagination.Limit).Order(
+			fmt.Sprintf("%s %s", filter.OrderBy, filter.Sort),
+		).Where("to_tsvector(body) @@ to_tsquery(?)", filter.Search)
 	}
 }
 
+// Check the entries and return the corrected ones.
 func GetPaginationFiltersFromQuery(filter *types.Filter, pagination *types.PaginationRequest) (*types.Pagination, *types.Filter) {
 	page := pagination.Page
 	limit := pagination.Limit
-
-	search := filter.Search
-	orderBy := filter.OrderBy
-	sort := filter.Sort
 
 	if page <= 0 {
 		page = 1
@@ -30,36 +40,31 @@ func GetPaginationFiltersFromQuery(filter *types.Filter, pagination *types.Pagin
 	if limit <= 0 {
 		limit = 5
 	}
-	if len(strings.TrimSpace(orderBy)) <= 0 {
-		orderBy = "updated_at"
+	if len(strings.TrimSpace(filter.OrderBy)) <= 0 {
+		filter.OrderBy = "updated_at"
 	}
-	if sort != "asc" {
-		sort = "desc"
+	if filter.Sort != "asc" {
+		filter.Sort = "desc"
 	}
 
-	return NewPaginationData(&page, &limit), NewFiltersData(&search, &orderBy, &sort)
+	return NewPaginationData(page, limit), filter
 }
 
-func NewPaginationData(page *int, limit *int) *types.Pagination {
+// The user passes a pagination request, specifying the desired page and limit.
+// We validate the inputs and return a new pagination object with the applied settings:
+// current page, next page, previous page, total pages, count, limit, and offset.
+func NewPaginationData(page int, limit int) *types.Pagination {
 	var offset = 0
-	if *page > 1 {
-		offset = (*page - 1) * *limit
+	if page > 1 {
+		offset = (page - 1) * limit
 	}
 	return &types.Pagination{
-		CurrentPage:  *page,
-		NextPage:     *page,
-		PreviousPage: *page,
+		CurrentPage:  page,
+		NextPage:     page,
+		PreviousPage: page,
 		TotalPages:   0,
 		Count:        0,
-		Limit:        *limit,
+		Limit:        limit,
 		Offset:       offset,
-	}
-}
-
-func NewFiltersData(search *string, orderBy *string, sort *string) *types.Filter {
-	return &types.Filter{
-		Search:  *search,
-		OrderBy: *orderBy,
-		Sort:    *sort,
 	}
 }
--- common/utils/validators.go
+++ common/utils/validators.go
@@ -9,19 +9,28 @@ import (
 	"github.com/4kpros/go-api/common/constants"
 )
 
+// Validate the authentication provider (e.g., Google, Facebook)
+// and return a boolean indicating success or failure.
 func IsAuthProviderValid(provider string) bool {
 	return slices.Contains(constants.AUTH_PROVIDERS, provider)
 }
 
+// Validate the phone number and return a boolean indicating success or failure.
 func IsPhoneNumberValid(phoneNumber uint64) bool {
 	return phoneNumber > 1000000
 }
 
+// Validate the email address and return a boolean indicating success or failure.
 func IsEmailValid(email string) bool {
 	emailAddress, err := mail.ParseAddress(email)
 	return err == nil && emailAddress.Address == email
 }
 
+// Validate the password and return a boolean indicating success or failure,
+// along with a string listing all missing requirements.
+//
+// For the password 'simplePass123!', the missing requirements are:
+// [HAS_MIN_LENGTH, HAS_UPPERCASE_LETTER, HAS_LOWERCASE_LETTER, HAS_NUMBER, and HAS_SPECIAL_CHARACTER].
 func IsPasswordValid(password string) (bool, string) {
 	var (
 		hasMinLen  bool
@@ -32,7 +41,7 @@ func IsPasswordValid(password string) (bool, string) {
 		missing    string
 	)
 
-	if len(password) >= 7 {
+	if len(password) >= 8 {
 		hasMinLen = true
 	}
 
@@ -52,7 +61,7 @@ func IsPasswordValid(password string) (bool, string) {
 	isValid := hasMinLen && hasUpper && hasLower && hasNumber && hasSpecial
 
 	if !isValid {
-		missing = fmt.Sprintf("[hasMinLen: %t, hasUpper: %t, hasLower: %t, hasNumber: %t, hasSpecial: %t]", hasMinLen, hasUpper, hasLower, hasMinLen, hasSpecial)
+		missing = fmt.Sprintf("HAS_MIN_LENGTH: %t, HAS_UPPERCASE_LETTER: %t, HAS_LOWERCASE_LETTER: %t, HAS_NUMBER: %t, HAS_SPECIAL_CHARACTER: %t]", hasMinLen, hasUpper, hasLower, hasNumber, hasSpecial)
 	}
 
 	return isValid, missing
