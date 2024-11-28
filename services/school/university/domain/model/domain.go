2c17e0e498e4ecb430c230b77ff444b27806fd7c
4kpros <prosper.abouar@gmail.com>
2024-11-28 23:53:28 +0100

    temp

---
+++ services/school/university/domain/model/domain.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"api/common/types"
+	"api/services/school/university/domain/data"
+)
+
+type Domain struct {
+	types.BaseGormModel
+	SchoolID     int64  `gorm:"not null"`
+	DepartmentID int64  `gorm:"not null"`
+	Name         string `gorm:"not null"`
+	Description  string `gorm:"not null"`
+}
+
+func (domain *Domain) ToResponse() *data.DomainResponse {
+	resp := &data.DomainResponse{}
+	resp.DepartmentID = domain.DepartmentID
+	resp.Name = domain.Name
+	resp.Description = domain.Description
+	return resp
+}
+
+func ToResponseList(domainList []Domain) []data.DomainResponse {
+	resp := make([]data.DomainResponse, len(domainList))
+	for index, domain := range domainList {
+		resp[index] = *domain.ToResponse()
+	}
+	return resp
+}
